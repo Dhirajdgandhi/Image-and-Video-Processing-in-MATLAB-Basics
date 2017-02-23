a621ffd73af18e145cb11f0e1b4e38e7deb7f787
Dhirajdgandhi <ddgandhi.96@gmail.com>
2017-02-23 22:23:19 +0530

    Add files via upload

---
+++ flipImage.m
@@ -0,0 +1,12 @@
+function plot = flipImage(img,direction)
+% This function rotates the image either vertically or horizontally
+% depending on the input parameters
+% img - Image and direction is 1 - horizontal ; 2 - vertical
+
+% Flip Image 
+flippedimg = flipdim(img,direction);
+
+subplot(2,2,1),imshow(img);
+title('Subplot 1: Original image');
+subplot(2,2,2),imshow(flippedimg);
+title('Subplot 2: Flipped image');
